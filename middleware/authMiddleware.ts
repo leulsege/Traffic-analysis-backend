6e63809270d3814a47b788d0f868a5cdba4b0ef5
leul <leulsegedgebremedhin@gmail.com>
2024-03-14 03:51:13 +0300

    commit

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -26,6 +26,7 @@ const createSendToken = (
           parseInt(process.env.JWT_COOKIE_EXPIRES_IN) * 24 * 60 * 60 * 1000,
       ),
       httpOnly: true,
+      secure: true,
     }
 
     res.cookie('token', token, cookieOptions)
